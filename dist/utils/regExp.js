deb43ab51c09f863543d2aa2693c09c396d8ecba
SnowDrive <luc.moralesr@gmail.com>
2023-02-26 15:10:34 -0300

    chore: add dist to .gitignore, remove dist folder

--- dist/utils/regExp.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RegExp = void 0;
-var RegExp;
-(function (RegExp) {
-    RegExp.jolinesUser = /!jolines @[a-zA-Z0-9_]{4,25}/;
-})(RegExp = exports.RegExp || (exports.RegExp = {}));
-//# sourceMappingURL=regExp.js.map
