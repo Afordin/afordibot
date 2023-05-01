2d58d5cfd2c7087b0fe84f1a0771d4a98538a94b
SnowDrive <luc.moralesr@gmail.com>
2023-05-01 15:03:59 -0400

    feat: add emoji service and tests

---
+++ src/domain/services/emoji.ts
@@ -0,0 +1,16 @@
+import { Aflor, Pig } from 'domain/types/Emoji'
+
+export class EmojiService {
+	public aflorOptions: Aflor[] = ['🌺', '🌻', '🌹', '🥀', '🌷', '🌼', '🌸', '💐', '🍄']
+	public pigOptions: Pig[] = ['🐷', '🐽', '🐖']
+
+	public getRandomAflor(): Aflor {
+		const randomIndex = Math.floor(Math.random() * this.aflorOptions.length)
+		return this.aflorOptions[randomIndex]
+	}
+
+	public getRandomPig(): Pig {
+		const randomIndex = Math.floor(Math.random() * this.pigOptions.length)
+		return this.pigOptions[randomIndex]
+	}
+}
