9d8afc0864ae65e342fc68239381b4db507ae2a8
SnowDrive <luc.moralesr@gmail.com>
2023-05-28 12:28:00 -0400

    feat: add use cases and text parser to awilix container

--- src/container.ts
+++ src/container.ts
@@ -2,13 +2,21 @@ import { createContainer, InjectionMode, asValue, asClass, asFunction } from 'aw
 import axios from 'axios'
 
 import { EmojiService } from 'domain/services/emoji'
+import { UserGeneratorService } from 'domain/services/userGenerator'
+
 import { AxiosHttpClient } from 'infrastructure/services/axiosHttpClient'
 import { RestHelixClient } from 'infrastructure/services/restHelixClient'
 import { CommandValidator } from 'infrastructure/services/commandValidator'
+import { TextParser } from 'infrastructure/services/textParser'
 import { FirebaseHandler } from 'infrastructure/persistance/firebase/dbHandler'
 import { userDocumentParser } from 'infrastructure/persistance/firebase/user/userDocumentParser'
 import { UserRepository } from 'infrastructure/persistance/firebase/user/userRepository'
-import { UserGeneratorService } from 'domain/services/userGenerator'
+
+import { GetChannelAflores } from 'application/getChannelAflores'
+import { GetChannelJolines } from 'application/getChannelJolines'
+import { GetUserJolines } from 'application/getUserJolines'
+import { IncrementAflores } from 'application/incrementAflores'
+import { IncrementJolines } from 'application/incrementJolines'
 
 import type { Dependencies } from 'types/container'
 
@@ -19,13 +27,19 @@ const container = createContainer<Dependencies>({
 container.register({
 	axios: asValue(axios),
 	emojiService: asClass(EmojiService).singleton(),
+	userGenerator: asClass(UserGeneratorService),
 	httpClient: asClass(AxiosHttpClient),
 	restHelixClient: asClass(RestHelixClient),
 	commandValidator: asClass(CommandValidator),
+	textParser: asClass(TextParser),
 	dbHandler: asClass(FirebaseHandler).singleton(),
 	userDocumentParser: asFunction(userDocumentParser),
 	userRepository: asClass(UserRepository),
-	userGenerator: asClass(UserGeneratorService),
+	getChannelAflores: asClass(GetChannelAflores),
+	getChannelJolines: asClass(GetChannelJolines),
+	getUserJolines: asClass(GetUserJolines),
+	incrementAflores: asClass(IncrementAflores),
+	incrementJolines: asClass(IncrementJolines),
 })
 
 export { container }
--- src/types/container.d.ts
+++ src/types/container.d.ts
@@ -1,21 +1,35 @@
 import { AxiosStatic } from 'axios'
 import { EmojiService } from 'domain/services/emoji'
+import { UserGeneratorService } from 'domain/services/userGenerator'
+
 import { AxiosHttpClient } from 'infrastructure/services/axiosHttpClient'
 import { RestHelixClient } from 'infrastructure/services/restHelixClient'
 import { CommandValidator } from 'infrastructure/services/commandValidator'
+import { TextParser } from 'infrastructure/services/textParser'
 import { FirebaseHandler } from 'infrastructure/persistance/firebase/dbHandler'
 import { UserDocumentParser } from 'infrastructure/types/firebase'
 import { UserRepository } from 'infrastructure/persistance/firebase/user/userRepository'
-import { UserGeneratorService } from 'domain/services/userGenerator'
+
+import { GetChannelAflores } from 'application/getChannelAflores'
+import { GetChannelJolines } from 'application/getChannelJolines'
+import { GetUserJolines } from 'application/getUserJolines'
+import { IncrementAflores } from 'application/incrementAflores'
+import { IncrementJolines } from 'application/incrementJolines'
 
 export interface Dependencies {
 	axios: AxiosStatic
 	emojiService: EmojiService
+	userGenerator: UserGeneratorService
 	httpClient: AxiosHttpClient
 	restHelixClient: RestHelixClient
 	commandValidator: CommandValidator
+	textParser: TextParser
 	dbHandler: FirebaseHandler
 	userDocumentParser: UserDocumentParser
 	userRepository: UserRepository
-	userGenerator: UserGeneratorService
+	getChannelAflores: GetChannelAflores
+	getChannelJolines: GetChannelJolines
+	getUserJolines: GetUserJolines
+	incrementAflores: IncrementAflores
+	incrementJolines: IncrementJolines
 }
