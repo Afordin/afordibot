f0f9b6ff88c635accd301f9f80b5440beda0a4f7
SnowDrive <luc.moralesr@gmail.com>
2023-06-04 17:58:47 -0400

    feat: add image entity

---
+++ src/domain/image/errors/invalidImageError.ts
@@ -0,0 +1,11 @@
+export class InvalidImageError extends Error {
+	constructor(message: string) {
+		super(message)
+	}
+}
+
+export enum InvalidImageMessages {
+	INVALID_USERNAME = 'Property "username" is required and must be a string with at least 4 characters',
+	INVALID_IMAGE_URL = 'Property "imageUrl" is required and must be a string',
+	INVALID_UPDATED_AT = 'Property "updatedAt" is required and must be a string with the format "DDMMYYYY"',
+}
