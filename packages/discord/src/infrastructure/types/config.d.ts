2ed8fd0cfd101b09957c21039b573e283e7947c4
SnowDrive <luc.moralesr@gmail.com>
2023-07-22 20:47:20 -0400

    feat: add command config, validations and tests

--- packages/discord/src/infrastructure/types/config.d.ts
+++ packages/discord/src/infrastructure/types/config.d.ts
@@ -1,4 +1,10 @@
+import type { Option } from 'infrastructure/config/option'
+
 export interface OptionConfig {
 	name: string
 	description?: string
+}
+
+export interface CommandConfig extends Required<OptionConfig> {
+	options: Option[]
 }
