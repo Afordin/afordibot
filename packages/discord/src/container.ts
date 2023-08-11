e5438e6d2550c293af731f1a017ed9c42210684f
SnowDrive <luc.moralesr@gmail.com>
2023-08-11 04:20:45 -0400

    feat: add config, command and option to awilix container

--- packages/discord/src/container.ts
+++ packages/discord/src/container.ts
@@ -1,6 +1,10 @@
 import { createContainer, InjectionMode, asValue } from 'awilix'
 import * as discordjs from 'discord.js'
 
+import { config } from 'infrastructure/config/env'
+import { Command } from 'infrastructure/config/command'
+import { Option } from 'infrastructure/config/option'
+
 import { Dependencies } from 'types/container'
 
 const container = createContainer<Dependencies>({
@@ -8,7 +12,13 @@ const container = createContainer<Dependencies>({
 })
 
 container.register({
+	// Libraries
 	discordjs: asValue(discordjs),
+
+	// Config
+	config: asValue(config),
+	command: asValue(Command),
+	option: asValue(Option),
 })
 
 export { container }
