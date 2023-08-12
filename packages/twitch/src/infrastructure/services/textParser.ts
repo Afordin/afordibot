f387ca57439e159b2872330cd5cf659f86f904b3
SnowDrive <luc.moralesr@gmail.com>
2023-08-12 04:49:28 -0400

    feat: add parse command name method to text parser service

--- packages/twitch/src/infrastructure/services/textParser.ts
+++ packages/twitch/src/infrastructure/services/textParser.ts
@@ -7,6 +7,10 @@ export class TextParserService {
 		return message.trim().toLowerCase()
 	}
 
+	public parseCommandName(message: string): string {
+		return message.split(' ')[0].replace('!', '')
+	}
+
 	public parseUsername(message: string): string {
 		return message.split(' ')[1].replace('@', '')
 	}
